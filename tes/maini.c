3fed56f19890e6c4b534ebbba47d6cfd45d04f43
martimpereira <61324221+martimpereira@users.noreply.github.com>
2020-03-18 09:55:41 +0000

    Versao funcional_18-3-20

--- tes/maini.c
+++ tes/maini.c
@@ -4,19 +4,12 @@
 
 #define RAND01 ((double)rand() / (double)RAND_MAX)
 
-
-typedef struct Matrix_Struct{
-    double** mz;
-    int num_l;
-    int num_c;
-}Matrix;
-
-void createMatrix(Matrix* _mz, int num_rows, int num_columns);
+void createMatrix(double*** _mz, int num_rows, int num_columns);
 void randomFillLR(int nU, int nI, int nF);
-void copyMatrix(Matrix* mz_src, Matrix* mz_dest);
-void swapMatrices(Matrix* a, Matrix* b);
-void multMatrices(Matrix* a, Matrix* b, Matrix* c);
-void factorization(Matrix* l,Matrix* r);
+void copyMatrix();
+void multMatrices();
+void factorization();
+void swapMatrices();
 
 int max_iterations = 0;
 double alpha = 0;
@@ -25,13 +18,12 @@ int num_l = 0;
 int num_c = 0;
 int non_zero_entries = 0;
 
-Matrix mz_a = {.mz = NULL};
-Matrix mz_l =  {.mz = NULL};
-Matrix mz_r =  {.mz = NULL};
-Matrix mz_b =  {.mz = NULL};
-
-Matrix mz_l_prev =  {.mz = NULL};
-Matrix mz_r_prev =  {.mz = NULL};
+double** mz_a = NULL;
+double** mz_l = NULL;
+double** mz_r = NULL;
+double** mz_b = NULL;
+double** mz_l_prev = NULL;
+double** mz_r_prev = NULL;
 
 int main(int argc, char* argv[]){
     FILE* fp = NULL;
@@ -43,143 +35,189 @@ int main(int argc, char* argv[]){
         fprintf(stderr, "Erro nos argumentos");
         exit(-1);
     }
-
+    
     fp = openFile(argv[1]);
     
     fscanf(fp, "%d\n", &max_iterations);
     fscanf(fp, "%lf\n", &alpha);
     fscanf(fp, "%d\n", &num_fs);
     fscanf(fp, "%d %d %d", &num_l, &num_c, &non_zero_entries);
+       
+    createMatrix(&mz_a, num_l, num_c);//Create matrix A
+    createMatrix(&mz_l, num_l, num_fs);//Create matrix L
+    createMatrix(&mz_r, num_fs, num_c);//Create matrix R
+    createMatrix(&mz_b, num_l, num_c);//Create matrix B
+    createMatrix(&mz_l_prev, num_l, num_fs);//Create matrix L_p
+    createMatrix(&mz_r_prev, num_fs, num_c);//Create matrix R_p
     
-    
-    
-    createMatrix(&mz_a, num_l, num_c);
-    createMatrix(&mz_l, num_l, num_fs);
-    createMatrix(&mz_r, num_fs, num_c);
-    createMatrix(&mz_l_prev, num_l, num_fs);
-    createMatrix(&mz_r_prev, num_fs, num_c);
-
     while(!feof(fp) && fscanf(fp, "%d %d %lf" , &lin, &col, &value)){
-        mz_a.mz[lin][col] = value;
+        mz_a[lin][col] = value;
     }
-    
-    randomFillLR(num_l, num_c, num_fs);
-    copyMatrix(&mz_l, &mz_l_prev);
-    copyMatrix(&mz_r, &mz_r_prev);
-    for(int a = 0; a < max_iterations; a++){
-        multMatrices(&mz_l,&mz_r,&mz_b);
-        factorization(&mz_l,&mz_r);
-        swapMatrices(&mz_l,&mz_l_prev);
-        swapMatrices(&mz_r,&mz_r_prev);
-        //imprimir para vermos as itera?oes ------------------------------------------
+    randomFillLR(num_l, num_c, num_fs);//L and R ramdom fill in. Copy to L_p e R_p
+    multMatrices();//B initial
+    //------------------------------------------------
+            // fprintf(stdout, "A\n");
+            // for(int i = 0; i < num_l; i++){
+                // for(int j = 0; j < num_c; j++){
+                    // fprintf(stdout, "%.3f ", mz_a[i][j]);
+                // }
+                // fprintf(stdout, "\n");
+            // }
+            // fprintf(stdout, "\n");
+            
+            
+    for(int a = 0; a < max_iterations; a++){  
+        factorization();
     }
-    //Só imprime------------------------------
-    for(int i = 0; i < num_l; i++){
-        for(int j = 0; j < num_c; j++){
-            fprintf(stdout, "%.3lf ", mz_a.mz[i][j]);
-        }
-        fprintf(stdout, "\n");
-    }
-    fprintf(stdout, "\n");
-
+    fprintf(stdout, "B\n");
+            for(int i = 0; i < num_l; i++){
+                for(int j = 0; j < num_c; j++){
+                    fprintf(stdout, "%f ", mz_b[i][j]);
+                }
+                fprintf(stdout, "\n");
+            }
+            fprintf(stdout, "\n");
     fclose(fp);
+    return 0;
 }
 
-void createMatrix(Matrix* _mz, int num_rows, int num_columns){
-
-    _mz->num_l = num_rows;
-    _mz->num_c = num_columns;
-
-    _mz->mz = (double**)malloc(num_rows * sizeof(double*));
-    if(_mz->mz == NULL){
-        fprintf(stderr, "Erro alocar memoria");
+void createMatrix(double*** _mz, int num_rows, int num_columns){
+    *_mz = (double**)malloc(num_rows * sizeof(double*));
+    if(*_mz == NULL){
+        fprintf(stderr, "Erro alocar memoria - linhas\n");
         exit(-1);
     }
     for(int a = 0; a < num_rows; a++){
-        _mz->mz[a] = (double*)malloc(num_columns * sizeof(double));
-        if(_mz->mz[a] == NULL){
-            fprintf(stderr, "Erro alocar memoria");
+        (*_mz)[a] = (double*)malloc(num_columns * sizeof(double));
+        if((*_mz)[a] == NULL){
+            fprintf(stderr, "Erro alocar memoria - colunas\n");
             exit(-1);
         }
     }
-
     for(int i = 0; i < num_rows; i++){
         for(int j = 0; j < num_columns; j++){
-            _mz->mz[i][j] = 0;
+            (*_mz)[i][j] = 0;
         }
     }
 }
 
 void randomFillLR(int nU, int nI, int nF){
     srand(0);
-
     for(int i = 0; i < nU; i++){
         for(int j = 0; j < nF; j++)
-            mz_l.mz[i][j] = RAND01 / (double) nF;
-    }
-        
+            mz_l[i][j] = RAND01 / (double) nF;
+    }  
     for(int i = 0; i < nF; i++){
         for(int j = 0; j < nI; j++)
-            mz_r.mz[i][j] = RAND01 / (double) nF;
+            mz_r[i][j] = RAND01 / (double) nF;
     }
+    copyMatrix();
 }
 
-void copyMatrix(Matrix* mz_src, Matrix* mz_dest){
-    for(int i = 0; i < mz_src->num_l; i++){
-        for(int j = 0; j < mz_src->num_c; j++){
-            mz_dest->mz[i][j] = mz_src->mz[i][j];
+void copyMatrix(){
+    for(int i = 0; i < num_l; i++){
+        for(int j = 0; j < num_fs; j++){
+            mz_l_prev[i][j] = mz_l[i][j];
+        }
+    }
+    for(int i = 0; i < num_fs; i++){
+        for(int j = 0; j < num_c; j++){
+            mz_r_prev[i][j] = mz_r[i][j];
         }
     }
 }
 
-void swapMatrices(Matrix* a, Matrix* b){
-    double** backup;
-
-    backup = a->mz;
-
-    a->mz = b->mz;
-    b->mz = backup;
-}
-
-void multMatrices(Matrix* a, Matrix* b, Matrix* c){
+void multMatrices(){
     double sum = 0;
-    int n=a->num_c;
-    int m=a->num_l;
-    int q=b->num_c;
-    int p=b->num_l;
-    if (n != p){
-        fprintf(stderr, "Erro multiplacar matrizes L e R");
-        exit(-1);
-    }else{
-        for (int e = 0; e < m; e++) {
-          for (int d = 0; d < q; d++) {
-            for (int k = 0; k < p; k++) {
-                sum = sum + a->mz[e][k]*b->mz[k][d];
+    for (int e = 0; e < num_l; e++) {
+        for (int d = 0; d < num_c; d++) {
+            for (int k = 0; k < num_fs; k++) {
+                sum = sum + (mz_l[e][k])*(mz_r[k][d]);
             }
-            c->mz[e][d] = sum;
+            mz_b[e][d] = sum;
             sum = 0;
-          }
         }
-    }
+    }  
 }
 
-void factorization(Matrix* l,Matrix* r){
+void swapMatrices(){
+    double** backup_l = mz_l_prev;
+    double** backup_r = mz_r_prev;
+    mz_l_prev=mz_l;
+    mz_r_prev=mz_r;
+    mz_l=backup_l;
+    mz_r=backup_r; 
+}
+
+void factorization(){
     double sum_l=0;
     double sum_r=0;
-    for(int i = 0; i < mz_a.num_l; i++){
-        for(int j = 0; j < mz_a.num_c; j++){
-            if (mz_a.mz[i][j] != 0){
-                for(int k = 0; k < mz_a.num_c; k++){
-                    for(int n = 0; n < mz_a.num_c; n++){
-                        sum_l = sum_l + 2 * (mz_a.mz[i][n] - mz_b.mz[i][n]) * (-1 * mz_r_prev.mz[k][n]); 
-                        sum_r = sum_r + 2 * (mz_a.mz[n][j] - mz_b.mz[n][j]) * (-1 * mz_l_prev.mz[n][k]); 
+    for(int i = 0; i < num_l; i++){
+        for(int j = 0; j < num_c; j++){
+            if (mz_a[i][j] != 0){
+                for(int k = 0; k < num_fs; k++){
+                    for(int n = 0; n < num_c; n++){
+                        if (mz_a[i][n] != 0){
+                            sum_l = sum_l + 2 * (mz_a[i][n] - mz_b[i][n]) * (-1 * mz_r_prev[k][n]);
+                        }
                     }
-                    l->mz[i][k] = mz_l_prev.mz[i][k] - alpha * sum_l;
+                    for(int n = 0; n < num_l; n++){
+                        if (mz_a[n][j] != 0){
+                            sum_r = sum_r + 2 * (mz_a[n][j] - mz_b[n][j]) * (-1 * mz_l_prev[n][k]);
+                        }                            
+                    }
+                    mz_l[i][k] = mz_l_prev[i][k] - alpha * sum_l;
                     sum_l=0;
-                    r->mz[k][j] = mz_r_prev.mz[k][j] - alpha * sum_r;
+                    mz_r[k][j] = mz_r_prev[k][j] - alpha * sum_r;
                     sum_r=0;
                 }
             }
         }
     }
+    multMatrices();
+    //---------------------------------------------------------------------------------------------------
+    // fprintf(stdout, "L\n");
+    // for(int i = 0; i < num_l; i++){
+    // for(int j = 0; j < num_fs; j++){
+            // fprintf(stdout, "%f ", mz_l[i][j]);
+        // }
+        // fprintf(stdout, "\n");
+    // }
+    // fprintf(stdout, "\n");
+    // fprintf(stdout, "R\n");
+    // for(int i = 0; i < num_fs; i++){
+        // for(int j = 0; j < num_c; j++){
+            // fprintf(stdout, "%f ", mz_r[i][j]);
+        // }
+        // fprintf(stdout, "\n");
+    // }
+    // fprintf(stdout, "\n");
+    // fprintf(stdout, "B\n");
+    // for(int i = 0; i < num_l; i++){
+        // for(int j = 0; j < num_c; j++){
+            // fprintf(stdout, "%f ", mz_b[i][j]);
+        // }
+        // fprintf(stdout, "\n");
+    // }
+    // fprintf(stdout, "\n");
+    // fprintf(stdout, "L_p\n");
+    // for(int i = 0; i < num_l; i++){
+        // for(int j = 0; j < num_fs; j++){
+            // fprintf(stdout, "%f ", mz_l_prev[i][j]);
+        // }
+        // fprintf(stdout, "\n");
+    // }
+    // fprintf(stdout, "\n");
+    // fprintf(stdout, "R_p\n");
+    // for(int i = 0; i < num_fs; i++){
+        // for(int j = 0; j < num_c; j++){
+            // fprintf(stdout, "%f ", mz_r_prev[i][j]);
+        // }
+        // fprintf(stdout, "\n");
+    // }
+    // fprintf(stdout, "\n");
+    //-----------------------------------------------------------------------------------------------------------    
+        
+    swapMatrices();
 }
+
